57c93ffb3a9b39b04ce9dde4b322b3f7e9991aab
Victoria Blatkowska <victoriablatkowska@gmail.com>
2020-09-30 11:08:07 +0200

    Course information step6

---
+++ part2/course-info/src/index.js
@@ -0,0 +1,83 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Course course={course} />
+
+      {/* <Total parts={course.parts} /> */}
+    </div>
+  );
+};
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+    </div>
+  );
+};
+
+const Header = ({ course }) => {
+  return (
+    <>
+      <h1>{course}</h1>
+    </>
+  );
+};
+const Content = ({ parts }) => {
+  return parts.map((part) => <Part key={part.id} info={part} />);
+};
+const Part = ({ info }) => {
+  return (
+    <>
+      <p>
+        {info.name} {info.exercises}
+      </p>
+    </>
+  );
+};
+
+// const Total = (props) => {
+//   return (
+//     <>
+//       <p>
+//         Number of exercises{" "}
+//         {props.parts[0].exercises +
+//           props.parts[1].exercises +
+//           props.parts[2].exercises}
+//       </p>
+//     </>
+//   );
+// };
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
